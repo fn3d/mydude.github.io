a4058330fd36b807b928bd692df192f1b7e1f6f6
Muhammad Farhan <farhan3d@gmail.com>
2024-09-27 13:41:43 +0200

    adjust tabs

--- react-test-1/src/App.js
+++ react-test-1/src/App.js
@@ -7,97 +7,95 @@ import { SceneProvider, SceneSetter, useScene } from './scene/sceneContext';
 import { createPrimitive } from './scene/geom';
 
 function PanelButton({buttonString, onClick}) {
-  return (
-    <button className="panelButton" onClick={() => onClick(buttonString)}>
-      { buttonString }
-    </button>
-  );
+	return (
+		<button className="panelButton" onClick={() => onClick(buttonString)}>
+		{ buttonString }
+		</button>
+	);
 }
 
 function PerformActionOnClick(buttonString) {
-
-  switch (buttonString) {
-    case "1":
-      return [createPrimitive("box"), "Box"];
-    case "2":
-      return [createPrimitive("sphere"), "Sphere"];
-    case "3":
-      return [createPrimitive("cylinder"), "Cylinder"];
-    case "4":
-      return [createPrimitive("cone"), "Cone"];
-  }
+	switch (buttonString) {
+		case "1":
+		return [createPrimitive("box"), "Box"];
+		case "2":
+		return [createPrimitive("sphere"), "Sphere"];
+		case "3":
+		return [createPrimitive("cylinder"), "Cylinder"];
+		case "4":
+		return [createPrimitive("cone"), "Cone"];
+	}
 }
 
 function MainPanel() {
-  return (
-    <SceneProvider>
-      <div className="mainPanel">
-        <ButtonsContainer />
-      </div>
-    </SceneProvider>
-  );
+	return (
+		<SceneProvider>
+		<div className="mainPanel">
+			<ButtonsContainer />
+		</div>
+		</SceneProvider>
+	);
 }
 
 function ButtonsContainer() {
+	const [stateVar, setState] = useState("Box");
+	const [primitive, setPrimitive] = useState(createPrimitive("box"));
+	const buttonLabels = ["1", "2", "3", "4"];
 
-  const [stateVar, setState] = useState("Box");
-  const [primitive, setPrimitive] = useState(createPrimitive("box"));
-  const buttonLabels = ["1", "2", "3", "4"];
+	const handleClick = (buttonString) => {
+		const [newPrimitive, desiredStateVal] = PerformActionOnClick(buttonString);
+		setState(desiredStateVal);
+		setPrimitive(newPrimitive);
+	}
 
-  const handleClick = (buttonString) => {
-    const [newPrimitive, desiredStateVal] = PerformActionOnClick(buttonString);
-    setState(desiredStateVal);
-    setPrimitive(newPrimitive);
-  }
-
-  return (
-    <>
-      <div className="buttonsContainer">
-        <div className="statusField">
-          { stateVar }
-        </div>
-        {buttonLabels.map((label, index) => (
-          <PanelButton
-            key={index}
-            buttonString={label}
-            onClick={handleClick}
-          />
-        ))}
-      </div>
-      <CanvasContainer primitive={primitive} />
-    </>
-  );
+	return (
+		<>
+		<div className="buttonsContainer">
+			<div className="statusField">
+			{ stateVar }
+			</div>
+			{buttonLabels.map((label, index) => (
+			<PanelButton
+				key={index}
+				buttonString={label}
+				onClick={handleClick}
+			/>
+			))}
+		</div>
+		<CanvasContainer primitive={primitive} />
+		</>
+	);
 }
 
 function CanvasContainer({primitive}) {
-  <SceneSetter />
-  return (
-    <div className="canvasContainer">
-      {
-        <StrictMode>
-          <Canvas camera={{ position: [5, 3, 5], fov: 35 }} gl={{ antialias: false }} shadows>
-            { primitive }
-            <OrbitControls />
-            <ambientLight intensity={1.0} />
-            <directionalLight position={[3, 2, 5]} intensity={1.5} castShadow />
-            <directionalLight position={[-3, 2, -5]} intensity={1.0} />
-            <directionalLight position={[-3, 6, 5]} intensity={1.0} />
-          </Canvas>
-        </StrictMode>
-      }
-    </div>
-  );
+	<SceneSetter />
+	return (
+		<div className="canvasContainer">
+		{
+			<StrictMode>
+			<Canvas camera={{ position: [5, 3, 5], fov: 35 }} gl={{ antialias: false }} shadows>
+				{ primitive }
+				<OrbitControls />
+				<ambientLight intensity={1.0} />
+				<directionalLight position={[3, 2, 5]} intensity={1.5} castShadow />
+				<directionalLight position={[-3, 2, -5]} intensity={1.0} />
+				<directionalLight position={[-3, 6, 5]} intensity={1.0} />
+			</Canvas>
+			</StrictMode>
+		}
+		</div>
+	);
 }
 
 export default function App() {
-  return (
-    <div className='body'>
-      <div className='mainContainer'>
-        <h1>
-          Welcome to the site, my dude.
-        </h1>
-        <MainPanel />
-      </div>
-    </div>
-  );
+	return (
+		<div className='body'>
+		<div className='mainContainer'>
+			<h1>
+			Welcome to the site, my dude.
+			</h1>
+			<MainPanel />
+		</div>
+		</div>
+	);
 }
